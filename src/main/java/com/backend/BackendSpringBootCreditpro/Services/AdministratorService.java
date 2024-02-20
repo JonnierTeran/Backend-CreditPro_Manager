8be8c0f62c312bf34093dbff0c426107eb36b37e
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-02-19 22:39:15 -0500

    Se agregaron validaciones al metodo de registrar administrador

--- src/main/java/com/backend/BackendSpringBootCreditpro/Services/AdministratorService.java
+++ src/main/java/com/backend/BackendSpringBootCreditpro/Services/AdministratorService.java
@@ -19,37 +19,52 @@ public class AdministratorService {
     AdministratorRepository _AdministratorRepository;
 
     /**
+     * GUARDAR UN ADMINISTRADOR
+     * se valida que no exista otro con el mismo id
+     * se valida que no exista otro con el mismo user
+     * 
      * @param Admin
      * @return Objeto Response (Meensaje)
      */
     public Response SaveAdministrator(AdministratorModel Admin) {
 
-        if (Admin != null) {
+        //Validacion que todas las propiedades del objeto sean diligenciadas
+        if (Admin.getIdentificacion() > 0 && !Admin.getName().isEmpty() && !Admin.getLastName().isEmpty()
+                && !Admin.getUser().isEmpty() && !Admin.getPassword().isEmpty()) {
 
-            // Validacion de que no exista un admin con el mismo user ingresado
-            if (this.GetAdministrator(Admin.getUser()) == null) {
+            // Validacion de que no exista un administador con el mismo id que se intenta
+            // registrar
+            if (this.GetAdministratorIdentificacion(Admin.getIdentificacion()) == null) {
 
-                // Metodo Save de JpaRepository para realizar un registro en la base de datos
-                // que retorna un JSON del objeto registrado
-                AdministratorModel AdminAux = this._AdministratorRepository.save(Admin);
+                // Validacion de que no exista un admin con el mismo user ingresado
+                if (this.GetAdministrator(Admin.getUser()) == null) {
 
-                // El metodo save regresa el objeto registrado en la bd, si no es null ha sido
-                // registrado con exito
-                if (AdminAux != null) {
-                    return new Response("Administrador Registrado Exitosamente");
-                } else {
-                    return new Response("Error Al registrar Administrador");
+                    // Metodo Save de JpaRepository para realizar un registro en la base de datos
+                    // que retorna un JSON del objeto registrado
+                    AdministratorModel AdminAux = this._AdministratorRepository.save(Admin);
+
+                    // El metodo save regresa el objeto registrado en la bd, si no es null ha sido
+                    // registrado con exito
+                    if (AdminAux != null) {
+                        return new Response("Administrador Registrado Exitosamente");
+                    } else {
+                        return new Response("Algo ha salido mal, error Al registrar Administrador, Intnete nuevamente");
+                    }
                 }
-            } 
-            // En caso que si exista un admin con el mismo user que se quiere regustrar
-            else {
-                return new Response("Ya Existe un Administrados Asociado al usuario " + Admin.getUser());
+                // En caso que si exista un admin con el mismo user que se quiere regustrar
+                else {
+                    return new Response("Ya Existe un Administrados Asociado al usuario " + Admin.getUser());
+                }
+
+            } else {
+                return new Response(
+                        "Ya Existe un Administrados Asociado a la identificación:  " + Admin.getIdentificacion());
             }
 
             // si el Objeto que llega por parametro es null
         } else {
             return new Response(
-                    "No se puedo Registrar Administrado, por favor proporcione la Inofrmacion correctamente");
+                    "No se pudo Registrar el Administrador, por favor proporcione la Inofrmacion correctamente");
         }
     }
 
@@ -80,18 +95,18 @@ public class AdministratorService {
         }
     }
 
-
     /**
      * @Descripcion Metodo para buscar por identificacion un admin si es que existe.
      * @param Identificacion
      * @return Informacion del Abministrador con esa identificacion.
      */
-    public AdministratorModel GetAdministratorIdentificacion(Long Identificacion){
-          // Metodo para validar si el paramertro tiene caracteres validos
-          if (Identificacion > 0) {
+    public AdministratorModel GetAdministratorIdentificacion(Long Identificacion) {
+        // Metodo para validar si el paramertro tiene caracteres validos
+        if (Identificacion > 0) {
 
             // Tipo Optional porque puede o no existir un admin con esa identificacion
-            Optional<AdministratorModel> Administrator = this._AdministratorRepository.findByIdentificacion(Identificacion);
+            Optional<AdministratorModel> Administrator = this._AdministratorRepository
+                    .findByIdentificacion(Identificacion);
 
             // Verificar sy hay algo presente en el objeto opcional
             if (Administrator.isPresent()) {

b9395b0edd084e60326aa1a809de831b512afb3c
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-03-02 16:49:53 -0500

    correo

--- src/main/java/com/backend/BackendSpringBootCreditpro/Services/AdministratorService.java
+++ src/main/java/com/backend/BackendSpringBootCreditpro/Services/AdministratorService.java
@@ -2,12 +2,11 @@ package com.backend.BackendSpringBootCreditpro.Services;
 
 import java.util.Optional;
 
-import com.resend.*;
-import com.resend.core.exception.ResendException;
-import com.resend.services.emails.model.SendEmailRequest;
-import com.resend.services.emails.model.SendEmailResponse;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import com.backend.BackendSpringBootCreditpro.Models.AdministratorModel;
 import com.backend.BackendSpringBootCreditpro.Models.Response;
@@ -22,6 +21,10 @@ public class AdministratorService {
     // Instancia Automatica // Inyeccion de Dependencias
     @Autowired
     AdministratorRepository _AdministratorRepository;
+
+    // Instancia automatica // Inyeccion de dependencia para JAVAMAILSENDER
+    @Autowired
+    private JavaMailSender mailSender;
 
     // #region Registrar Administrador
 
@@ -139,8 +142,7 @@ public class AdministratorService {
 
     // #endregion
 
-
-    //#region Restaurar cuenta por email
+    // #region Restaurar cuenta por email
 
     /**
      * @Descripcion Metodo para recuperar una cuenta basado en un usuario registrado
@@ -153,36 +155,23 @@ public class AdministratorService {
 
         if (Admin != null) {
 
-            //Envio del correo utilizando la API DE RESEND MAILS
+            // Envio del correo utilizando JAVAMAILSENDER
 
-            Resend resend = new Resend("re_H9KAveXF_PiYg3yJMxVwZJ3zWZkae9kcz");
+            SimpleMailMessage email = new SimpleMailMessage();
+            email.setFrom("jonnierteranmorales@gmail.com");
+            email.setTo(User);
+            email.setSubject("SOPORTE TECNICO CEDITPRO MANAGER - RECUPERACIÓN DE CUENTA");
+            email.setText("prbando");
+            mailSender.send(email);
 
-            SendEmailRequest sendEmailRequest = SendEmailRequest.builder()
-                    .from("onboarding@resend.dev")
-                    .to(User)
-                    .subject("SOPORTE TECNICO CEDITPRO MANAGER - RECUPERACIÓN DE CUENTA")
-                    .html("<strong>Cordial saludo. <br> </strong> <br> Querido usuario  <strong>" + Admin.getName() + " "
-                            + Admin.getLastName()
-                            + ", </strong> el equipo de soporte tecnico se permite enviar a usted luego de solicitar el restablecimiento de su contraseña para ingresar al sofware <strong> CREDITPRO-MANAGER </strong> su password de acceso, se recomienda de forma obligatoria realizar un cambio de contraseña al ingresar al aplicativo web de forma urgente para garantizar seguridad y calidad en el mantenimiento de la información, recuerde que el email de acceso es el mismo de la cuenta en la que recibe este email.  <br> <br>  Contraseña de Acceso: "
-                            + Admin.getPassword()
-                            + " <br> <br>  Gracias por utilizar nuestros serviciós. <br> <hr> <strong> CREDITPRO-MANAGER </strong> <br> Si necestia soporte tecnico comunicarse al correo: <strong> jonnierteranmorales@gmail.com </strong>")
-                    .build();
-                    
-            try {
-                SendEmailResponse data = resend.emails().send(sendEmailRequest);
-                System.out.println(data.getId());
-                return new Response(
-                        "La información de recuperación de cuenta ha sido enviada exitosamente al email: " + User);
-            } catch (ResendException e) {
-                e.printStackTrace();
-                return new Response("Error al enviar el correo");
-            }
+            return new Response("El correo de recuperación de cuenta ha sido envado exitosamente al email: " + User);
+
         } else {
             return new Response("El usuario " + User + " No existe, verifique el email ingresado e intente nuevamente");
         }
 
     }
 
-    //#endregion
+    // #endregion
 
 }

f7a7bf983a2f452fe219caa18465e9852c676e2e
Jonnier Teran <jonnierteranmorales@gmail.com>
2024-03-03 17:49:32 -0500

    agregando nuevo api key al modelo y modificando metodo de restablecimiento

--- src/main/java/com/backend/BackendSpringBootCreditpro/Models/AdministratorModel.java
+++ src/main/java/com/backend/BackendSpringBootCreditpro/Models/AdministratorModel.java
@@ -5,76 +5,84 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 
-
 /**
  * Administrator model
  */
-@Entity  // Indicador de que es un modelo Real para una Base de datos
-@Table(name = "Administrator") // Indicamos el nombre que queremos para la tabla en la bd para que no use el de la clase
+@Entity // Indicador de que es un modelo Real para una Base de datos
+@Table(name = "Administrator") // Indicamos el nombre que queremos para la tabla en la bd para que no use el de
+                               // la clase
 public class AdministratorModel {
-    
-    //Propiedades de la entidas / clase
 
+    // Propiedades de la entidas / clase
 
-    @Id // Indicamos que es un id  primary key
-    @Column(unique = true, nullable = false) // Es unico  y no nullo
+    @Id // Indicamos que es un id primary key
+    @Column(unique = true, nullable = false) // Es unico y no nullo
     private Long identificacion;
 
-    @Column(nullable = false,  columnDefinition = "VARCHAR(250)" )  // Es unico  y no nullo, y tiene 255 de max long
+    @Column(nullable = false, columnDefinition = "VARCHAR(250)") // Es unico y no nullo, y tiene 255 de max long
     private String name;
 
-    @Column( nullable = false, columnDefinition = "VARCHAR(250)") // Es unico  y no nullo
+    @Column(nullable = false, columnDefinition = "VARCHAR(250)") // Es unico y no nullo
     private String lastName;
 
-    @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(250)") // Es unico  y no nullo
+    @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(250)") // Es unico y no nullo
     private String user;
 
-    @Column(nullable = false, columnDefinition = "VARCHAR(250)") // Es unico  y no nullo
+    @Column(nullable = false, columnDefinition = "VARCHAR(250)") // Es unico y no nullo
     private String password;
 
+    @Column(name = "apiKeyResend", nullable = true, columnDefinition = "VARCHAR(250)") // Es unico y no nullo
+    private String apiKeyResend;
 
-    public AdministratorModel(){}
-
-    public void setIdentificacion(Long identificacion){
-        this.identificacion=identificacion;
+    public AdministratorModel() {
     }
 
-    public Long getIdentificacion(){
-        return this.identificacion; 
+    public void setIdentificacion(Long identificacion) {
+        this.identificacion = identificacion;
     }
 
-    public void setName(String name){
+    public Long getIdentificacion() {
+        return this.identificacion;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public void setLastName(String lastName){
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public String getLastName(){
+    public String getLastName() {
         return this.lastName;
     }
 
-    public void setUser(String user){
-        this.user=user;
+    public void setUser(String user) {
+        this.user = user;
     }
 
-    public String getUser(){
+    public String getUser() {
         return this.user;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return this.password;
     }
 
+    public void setApiKeyResend(String apiKeyResend) {
+        this.apiKeyResend = apiKeyResend;
+    }
 
+    public String getApikeyResend() {
+        return this.apiKeyResend;
+    }
 
 }
